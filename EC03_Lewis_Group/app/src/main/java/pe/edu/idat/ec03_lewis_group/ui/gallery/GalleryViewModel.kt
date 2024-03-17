51c7c936e7352d3bdfa547bbabaa5bcc39ed9313
inoricode28 <inori237@gmail.com>
2024-03-16 19:07:26 -0500

    Base

---
+++ EC03_Lewis_Group/app/src/main/java/pe/edu/idat/ec03_lewis_group/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec03_lewis_group.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ EC03_Lewis_Group/app/src/main/java/pe/edu/idat/ec03_lewis_group/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec03_lewis_group.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
